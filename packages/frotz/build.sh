dd8395b39c87d954f849e7013813026001312347
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-05-21 20:57:30 +0300

    frotz: bump one more time
    After updating termux-packages submodule.

--- packages/frotz/build.sh
+++ packages/frotz/build.sh
@@ -4,7 +4,7 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 # frotz does not depend on dialog or curl, but the zgames script we bundle below in termux_step_make_install() do.
 TERMUX_PKG_DEPENDS="ncurses, dialog, curl"
 TERMUX_PKG_VERSION=2.44
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/DavidGriffith/frotz/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=dbb5eb3bc95275dcb984c4bdbaea58bc1f1b085b20092ce6e86d9f0bf3ba858f
 TERMUX_PKG_BUILD_IN_SRC=yes
