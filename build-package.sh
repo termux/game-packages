0685891939021ce92225a47551cdaa5a5abaa1d3
Henrik Grimler <henrik@grimler.se>
2019-10-24 12:28:50 +0200

    Update CI scripts to latest versions

--- build-package.sh
+++ build-package.sh
@@ -2,11 +2,5 @@
 
 set -e -o pipefail -u
 
-# link into main package repo to handle dependencies
-for package in packages/*; do
-    #TODO: handle docker image builds better
-    ln -sf ../../${package} termux-packages/packages/
-done
-
 cd termux-packages
-TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91" ./build-package.sh "$@"
+./build-package.sh "$@"
