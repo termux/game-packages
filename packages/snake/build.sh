b1a09e5610d1bb6a3c8afe911068853cdbd1de3c
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-09-07 21:40:10 +0800

    New package: snake

---
+++ packages/snake/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/TipzTeam/snake-cpp-termux
+TERMUX_PKG_DESCRIPTION="Play the 90s game again."
+TERMUX_PKG_LICENSE=MIT
+TERMUX_PKG_VERSION=1.0
+TERMUX_PKG_SRCURL=https://github.com/TipzTeam/snake-cpp-termux/archive/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=f174e9ef5cd581cbec3748e5d3d0c370dd819858e4126d01cdcb29cff93f6d0b
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_MAINTAINER="Tipz Team @TipzTeam"
+TERMUX_PKG_BUILD_DEPENDS="clang, make"
+
+termux_step_make_install() {
+        install -m755 snake $TERMUX_PREFIX/bin/
+}
