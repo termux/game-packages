5e16a8199d48f85347455932e3b0a29807260f3f
Tipz Team <47551974+TipzTeam@users.noreply.github.com>
2020-09-08 08:02:10 +0800

    Update build.sh

--- packages/snake/build.sh
+++ packages/snake/build.sh
@@ -1,9 +1,9 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/TipzTeam/snake-cpp-termux
+TERMUX_PKG_HOMEPAGE=https://github.com/gogakoreli/snake
 TERMUX_PKG_DESCRIPTION="Eat as much as you want while avoiding walls."
 TERMUX_PKG_LICENSE=MIT
-TERMUX_PKG_VERSION=1.0
-TERMUX_PKG_SRCURL=https://github.com/TipzTeam/snake-cpp-termux/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=f174e9ef5cd581cbec3748e5d3d0c370dd819858e4126d01cdcb29cff93f6d0b
+TERMUX_PKG_VERSION=2017.06.09
+TERMUX_PKG_SRCURL=https://github.com/gogakoreli/snake/archive/4ccdf33feaa0a24578f3ddcf8137e52c93444f15.tar.gz
+TERMUX_PKG_SHA256=f99212d620ac593272a0489a7d83b44f92a39bcd11c299c728ea08f1eee656a6
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_MAINTAINER="Tipz Team @TipzTeam"
 
