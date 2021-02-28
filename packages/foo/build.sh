fdda86da7773a026567c7af09a6c5b6fb665e28a
Henrik Grimler <henrik@grimler.se>
2021-02-28 21:13:30 +0100

    bump test package build

--- packages/foo/build.sh
+++ packages/foo/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="dummy package for testing subpackage handling by github 
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=28
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://github.com/termux/libandroid-support/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=ef35260994ffa3bd054be66068dfc28934c823ac8de2394796d94d1cd5de3be4
 
